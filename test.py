bd863a5ed7893735d78016a67872e616b4b61d5e
Sachin Krishnamurthy <sachinkrishnamurthy@Sachins-MacBook-Air.local>
2026-08-04 19:38:51 -0400

    Adding agents and ui

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
 # res = tavily_search("Best hotels in India?")
 # print(res)
 
-res = search_flights("Plan a 7 days Japan trip from Bangalore")
-print(res)
+# res = search_flights("Plan a 7 days Japan trip from Bangalore")
+# print(res)
+
+user_input = input("Enter travel request: ")
+response = run_travel_agent(user_input, "test_user_001")
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
